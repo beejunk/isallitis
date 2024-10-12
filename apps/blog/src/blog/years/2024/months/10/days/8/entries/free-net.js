70eac82c1f0436bd04b3fcdbe503307dbfa59690
Brian David <beejunk@gmail.com>
2024-10-12 07:49:34 -0500

    feat(blog): free-net updates

--- apps/blog/src/blog/years/2024/months/10/days/8/entries/free-net.js
+++ apps/blog/src/blog/years/2024/months/10/days/8/entries/free-net.js
@@ -10,17 +10,122 @@ const hour = 8;
 
 const minute = 11;
 
-const links = {
-  uDistributionRadiant: "https://www.reddit.com/user/Distribution-Radiant/",
-  redditComment:
-    "https://www.reddit.com/r/vintagecomputing/comments/159swu2/comment/jtig8le/",
-};
+const links = [
+  {
+    title: "Fight For Our Web",
+    url: "https://www.citationneeded.news/fighting-for-our-web/",
+  },
+  {
+    title: "WebCrawler on Wikipedia",
+    url: "https://en.wikipedia.org/wiki/WebCrawler",
+  },
+  {
+    title: "Free-Net on Wikipedia",
+    url: "https://en.wikipedia.org/wiki/Free-net",
+  },
+  {
+    title: "Reddit comment about the Rio Grande Free Net",
+    url: "https://www.reddit.com/r/vintagecomputing/comments/159swu2/comment/jtig8le/",
+  },
+  {
+    title: "Reddit user Distribution-Radiant",
+    url: "https://www.reddit.com/user/Distribution-Radiant/",
+  },
+];
 
 const body = () => html`
   <p>
-    And now, in a possibly vain attempt at keeping this history from dying of
-    link rot, I present the full Reddit post from
-    <${BlogLink} href=${links.uDistributionRadiant}>u/Distribution Radiant</${BlogLink}>.
+    Molly White recently gave <${BlogLink} href=${links[0].url}>a talk
+    called "Fighting For Our Web"</${BlogLink}>, where she asked:
+  </p>
+  
+  <blockquote>
+    <p>Do you remember the first time you felt like the web was magic?</p>
+  </blockquote>
+  
+  <p>
+    I found the talk to be inspiring, and this question in particular
+    brought about a bunch of warm fuzzy memories. The first time I really got to
+    use the web was when I was in middle school and my friends and I bussed over
+    to the public computer lab at the University of Texas at El Paso where you could
+    sign up for time to use the Internet. I immediately loaded up
+    <${BlogLink} href=${links[1].url}>WebCrawler</${BlogLink}> (which was the
+    fashion at the time) to search the web, and was hit with that magical feeling
+    when I realized that I could look up any guitar tab I wanted. For free. No
+    strings attached (well, with the exception that the tab was probably wildly
+    inaccurate).
+  </p>
+  
+  <p>
+    A year or so after these trips to UTEP, my grandparents gifted us a computer.
+    It ran Windows 3.11, although Windows 95 had just come out and was included
+    as an upgrade. As part of the gift we also got copies of WarCraft II
+    and Myst. I actually still use the power supply that came with that computer;
+    many of the bulbs in the switches still work!
+  </p>
+  
+  <p>
+    But that's not the important part. The important part is this: there was a modem.
+    And I was able to use this modem to connect to the Internet. For free. No
+    strings attached.
+  </p>
+  
+  <p>
+    Okay, well, some strings were attached. The "for free" part was because there was a
+    <${BlogLink} href=${links[2].url}>free-net</${BlogLink}> called the Rio Grande Free Net that was
+    run nearby at El Paso Community College by someone named Don Furth. And while
+    you could connect to it for free, it all ran in a terminal and so wasn't
+    exactly exciting to look at.
+  </p>
+ 
+  <p>
+    But it was exciting to use.
+  </p>
+  
+  <p>
+    Oh, and did I mention it was free? This was extremely important for a few reasons.
+    First, my family and I were poor. Second, the Rio Grand Free Net (and the concept
+    of free-nets in general) exposed me to the idea that there were people out there
+    who not only built the Internet for everyone to use, but who were dedicated to
+    making the Internet available to anyone, just because they thought that was
+    a right and good thing to do.
+  </p>
+  
+  <p>
+    I eventually did figure out a way to get the free-net hooked up to NetScape.
+    Which taught me another valuable lesson: if you were obsessive and dedicated
+    enough and willing to put up with a lot of hassle, then you could eventually
+    get computers to do mostly anything you'd like them to do.
+  </p>
+  
+  <p>
+    This sense that the Internet was for everyone and that computers were for
+    everyone - it was a nice feeling. It was different. It wasn't the type of thing
+    I ran into a lot as a poor kid. It wasn't something that you had to walk into
+    a store and look at from a distance, maybe take for a test run, but never really
+    get to use.
+  </p>
+  
+  <p>
+    That's what the Internet meant to me.
+  </p>
+  
+  <h2>Addendum</h2>
+  
+  <p>
+    The info I posted above about the Rio Grande Free Net was
+    found in <${BlogLink} href=${links[3].url}>a Reddit comment</${BlogLink}>,
+    which in this case is the likeliest place to find such a thing. As it turns
+    out, the modem that came with our computer got zapped by a thunder storm one day and
+    had to be replaced. I wonder if it was one of the storms described in the
+    comment.
+  </p>
+  
+  <p>
+    Anyway, in a possibly vain attempt at keeping this history from dying of link rot, I
+    present the full Reddit post from
+    <${BlogLink} href=${links[4].url}>u/Distribution Radiant</${BlogLink}>.
+    It's a nice post that's worth preserving.
   </p>
 
   <hr />
